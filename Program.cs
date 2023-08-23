b62ecd9bc66e318a74aef2e1d57af8e9286c0719
Alex Mangust <sanya.stogny@gmail.com>
2023-08-24 01:50:35 +0800

    Написали двумерный массив с рандомными числами

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[,] table = new string[2, 3];
+// // table[0,0] table[0,1] table[0,2] 
+// // table[1,0] table[1,1] table[1,2]
+// table[1, 2] = "Слово";
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for(int columns = 0; columns < 3; columns++)
+//     {
+//         System.Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+int[,] matrix = new int[3, 4];
+for(int i = 0; i < matrix.GetLength(0); i++) //Параметр 0 определяет, что это строки
+{
+    for(int j = 0; j < matrix.GetLength(1); j++) //Параметр 1 определяет, что это столбцы
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        System.Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
